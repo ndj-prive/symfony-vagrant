83904ecdf949ffe841cf2eb346ff644a908c393e
Ndj - MIA - mac <nico.dejonghe@mia.be>
2013-04-01 21:53:33 +0200

    added capifony tested also added behat not yet tested need to add node js and zommbiejs

--- manifests/app.pp
+++ manifests/app.pp
@@ -32,9 +32,30 @@ apache::module {'env': }
 apache::module {'deflate': }
 apache::module {'expires': }
 
+apache::vhost { 'projectname.dev':
+    docroot             => '/var/www/document_root/test',
+    template            => '/home/vagrant/code/resources/apache2-project-vhost',
+}
+
+host { "projectname.dev":
+  ip => "33.33.33.10",
+}
+
+host { "adminer.dev":
+  ip => "33.33.33.10",
+}
+
 class { "mysql": 
     root_password => root,
         require => Exec["apt-get-update"],
+}
+
+mysql::grant { 'iamhacked':
+  mysql_privileges => 'ALL',
+  mysql_password => 'root',
+  mysql_db => 'iamhacked',
+  mysql_user => 'iamhacked',
+  mysql_host => 'localhost',
 }
 
 class { 'php':     require => Exec["apt-get-update"]}
@@ -129,3 +150,89 @@ file {'apache2.default-vhost':
     source  => '/home/vagrant/code/resources/apache2-default-vhost',
     require => Php::Module['cli']
 }
+
+
+$tmp_path        = '/home/vagrant'
+$php_package     = 'php5-cli'
+
+exec { 'download_composer':
+      path => '/usr/bin:/usr/sbin:/bin',
+      command     => 'curl -s http://getcomposer.org/installer | php',
+      cwd         => "$tmp_path/code/web",
+      require     => [
+        Package['curl', $php_package],
+      ],
+      creates     => "$tmp_path/code/web/composer.phar",
+}
+
+exec { 'download_behat':
+      path => '/usr/bin:/usr/sbin:/bin',
+      command     => 'curl -O http://behat.org/downloads/symfony2_extension.phar',
+      cwd         => "$tmp_path/code/web",
+      require     => [
+        Package['curl', $php_package],
+      ],
+      creates     => "$tmp_path/code/web/symfony2_extension.phar",
+}
+
+exec { 'download_mink':
+      path => '/usr/bin:/usr/sbin:/bin',
+      command     => 'curl -O http://behat.org/downloads/mink.phar',
+      cwd         => "$tmp_path/code/web",
+      require     => [
+        Package['curl', $php_package],
+      ],
+      creates     => "$tmp_path/code/web/mink.phar",
+}
+
+exec { 'download_mink_extention':
+      path => '/usr/bin:/usr/sbin:/bin',
+      command     => 'curl -O http://behat.org/downloads/mink_extension.phar',
+      cwd         => "$tmp_path/code/web",
+      require     => [
+        Package['curl', $php_package],
+      ],
+      creates     => "$tmp_path/code/web/mink_extension.phar",
+}
+
+
+
+
+# Install rubygems
+package { "rubygems": ensure => latest }
+
+# Install capifony and add it to path
+package { "capifony":
+    ensure   => latest,
+    provider => gem,
+    require  => Package["rubygems"]
+}
+
+package { "capistrano-ext":
+    ensure   => latest,
+    provider => gem,
+    require  => Package["rubygems"]
+}
+
+cron { "backup db":
+  command => "/usr/bin/mysqldump -uiamhacked -proot iamhacked > $tmp_path/code/dump.sql",
+  user    => root,
+  hour    => '2',
+  minute  => '0'
+}
+
+exec { 'create-dir-adminer':
+    path => '/usr/bin:/usr/sbin:/bin',
+    command => "mkdir -p $tmp_path/code/web",
+    unless => "[ -d '/$tmp_path/code/web' ]",
+}
+
+#exec { 'download_adminer':
+#      path => '/usr/bin:/usr/sbin:/bin',
+#      command     => 'curl -O http://downloads.sourceforge.net/adminer/adminer-3.6.3.ph',
+#      cwd         => "$tmp_path/code/web/adminer/",
+#      require     => [
+#        Package['curl'],Exec["create-dir-adminer"]
+#      ],
+#      creates     => "tmp_path/code/web/adminer/adminer.php",
+#}
--- setup.bash
+++ setup.bash
@@ -1,42 +1,48 @@
 #!/usr/bin/env bash
 
-git submodule update --init
-git submodule update --recursive
+#git submodule update --init
+#git submodule update --recursive
 
 #create a standard vhost file ready to move
 PROJECTNAME=${PWD##*/}
 
-touch $PROJECTNAME
-echo "<Virtualhost *:80>" > "$PROJECTNAME"
-echo "" >> "$PROJECTNAME"
-echo "    ServerAdmin info@dejonghenico.be" > "$PROJECTNAME"
-echo "    ServerName $PROJECTNAME.dev" >> "$PROJECTNAME"
-echo "" >> "$PROJECTNAME"
-echo "    Documentroot /home/vagrant/code/web/$PROJECTNAME/web" >> "$PROJECTNAME"
-echo "" >> "$PROJECTNAME"
-echo "    <Directory />" >> "$PROJECTNAME"
-echo "        Options FollowSymLinks" >> "$PROJECTNAME"
-echo "        AllowOverride all" >> "$PROJECTNAME"
-echo "    </Directory>" >> "$PROJECTNAME"
-echo "    <Directory /home/vagrant/code/web/$PROJECTNAME/web>" >> "$PROJECTNAME"
-echo "        Options Indexes FollowSymLinks MultiViews" >> "$PROJECTNAME"
-echo "        #DirectoryIndex index.php" >> "$PROJECTNAME"
-echo "        AllowOverride all" >> "$PROJECTNAME"
-echo "        Order allow,deny" >> "$PROJECTNAME"
-echo "        allow from all" >> "$PROJECTNAME"
-echo "    </Directory>" >> "$PROJECTNAME"
-echo "" >> "$PROJECTNAME"
-echo "    ErrorLog /var/log/apache2/error.log" >> "$PROJECTNAME"
-echo "" >> "$PROJECTNAME"
-echo "    # Possible values include: debug, info, notice, warn, error, crit," >> "$PROJECTNAME"
-echo "    # alert, emerg." >> "$PROJECTNAME"
-echo "    LogLevel warn" >> "$PROJECTNAME"
-echo "" >> "$PROJECTNAME"
-echo "    CustomLog /var/log/apache2/access.log combined" >> "$PROJECTNAME"
-echo "" >> "$PROJECTNAME"
-echo "</VirtualHost>" >> "$PROJECTNAME"
+VHOSTPATH="resources/apache2-project-vhost"
+
+touch $VHOSTPATH
+echo "<Virtualhost *:80>" > $VHOSTPATH
+echo "" >> $VHOSTPATH
+echo "   SetEnv FLATCLUB_ENV dev" >> $VHOSTPATH
+echo "   SetEnv FLATCLUB_DEBUG true" >> $VHOSTPATH
+echo "" >> $VHOSTPATH
+echo "    ServerAdmin info@dejonghenico.be" >> $VHOSTPATH
+echo "    ServerName $PROJECTNAME.dev" >> $VHOSTPATH
+echo "" >> $VHOSTPATH
+echo "    Documentroot /home/vagrant/code/web/$PROJECTNAME/web" >> $VHOSTPATH
+echo "" >> $VHOSTPATH
+echo "    <Directory />" >> $VHOSTPATH
+echo "        Options FollowSymLinks" >> $VHOSTPATH
+echo "        AllowOverride all" >> $VHOSTPATH
+echo "    </Directory>" >> $VHOSTPATH
+echo "    <Directory /home/vagrant/code/web/$PROJECTNAME/web>" >> $VHOSTPATH
+echo "        Options Indexes FollowSymLinks MultiViews" >> $VHOSTPATH
+echo "        #DirectoryIndex index.php" >> $VHOSTPATH
+echo "        AllowOverride all" >> $VHOSTPATH
+echo "        Order allow,deny" >> $VHOSTPATH
+echo "        allow from all" >> $VHOSTPATH
+echo "    </Directory>" >> $VHOSTPATH
+echo "" >> $VHOSTPATH
+echo "    ErrorLog /var/log/apache2/error.log" >> $VHOSTPATH
+echo "" >> $VHOSTPATH
+echo "    # Possible values include: debug, info, notice, warn, error, crit," >> $VHOSTPATH
+echo "    # alert, emerg." >> $VHOSTPATH
+echo "    LogLevel warn" >> $VHOSTPATH
+echo "" >> $VHOSTPATH
+echo "    CustomLog /var/log/apache2/access.log combined" >> $VHOSTPATH
+echo "" >> $VHOSTPATH
+echo "</VirtualHost>" >> $VHOSTPATH
 
 git clone git://github.com/ndj-prive/git.git git-hooks
+
 
 cd vagrant
 vagrant up
