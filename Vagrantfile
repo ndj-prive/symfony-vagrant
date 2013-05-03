0953b7c60cf023f2f93a183d2c409ea3c3e6c90d
Ndj - MIA - mac <nico.dejonghe@mia.be>
2013-05-03 10:41:13 +0200

    firsttime drupal setup

--- Vagrantfile
+++ Vagrantfile
@@ -1,14 +1,14 @@
 # http://vagrantup.com/v1/docs/vagrantfile.html
 
 Vagrant::Config.run do |config|
-  config.vm.box = "base"
+  config.vm.box = "drupal"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
   #config.vm.box = 'centos-62-64-puppet'
   #config.vm.box_url = 'http://packages.vstone.eu/vagrant-boxes/centos/6.2/centos-6.2-64bit-puppet-vbox.4.1.12.box'
   config.vm.define :project do |project_config|
-      project_config.vm.host_name = "dejonghenico.dev"
+      project_config.vm.host_name = "drupal-dev"
 
-      config.vm.network :hostonly, "33.33.33.10"
+      config.vm.network :hostonly, "33.33.33.15"
       config.vm.customize ["modifyvm", :id, "--memory", 1024]
       config.vm.share_folder "v-root", "/home/vagrant/code", ".", :nfs => true
 
--- manifests/app.pp
+++ manifests/app.pp
@@ -24,6 +24,24 @@ package{
  'acl':
     ensure  => present
 }
+package{
+ 'wget':
+    ensure  => present
+}
+package{
+ 'drush':
+    ensure  => present,
+    require => Exec['apt-get-update']
+}
+package{
+ 'openjdk-7-jre-headless':
+    ensure  => present,
+    require => Exec['apt-get-update']
+}
+package{
+ 'tar':
+    ensure  => present
+}
 class {
  'apache': 
     require => Exec['apt-get-update'],
@@ -100,6 +118,7 @@ exec {
     command => '/usr/bin/pear install pear.phpdoc.org/phpDocumentor-alpha --alldeps',
     require => [Php::Module['cli'], Exec['pear-auto-discover'], Exec['pear-update']]
 }
+
 file {
  '/home/vagrant/code/web':
     ensure  => 'directory',
@@ -141,6 +160,10 @@ file {
  '/home/vagrant/code/web/adminer':
     ensure  => 'directory',
 }
+file {
+ '/home/vagrant/servers':
+    ensure  => 'directory',
+}
 exec{
  'download_adminer':
     path    => '/usr/bin:/usr/sbin:/bin',
@@ -155,19 +178,53 @@ file {
     source  => '/home/vagrant/code/resources/apache2-adminer-vhost',
     require => File['/home/vagrant/code/web'],
 }
+file {
+ 'apache2.solrtest-vhost':
+    path    => '/etc/apache2/sites-available/solrtest',
+    ensure  => present,
+    source  => '/home/vagrant/code/resources/apache2-solrtest-vhost',
+    require => File['/home/vagrant/code/web'],
+}
+file {
+ 'apache2.solrtest2-vhost':
+    path    => '/etc/apache2/sites-available/solrtest2',
+    ensure  => present,
+    source  => '/home/vagrant/code/resources/apache2-solrtest2-vhost',
+    require => File['/home/vagrant/code/web'],
+}
 host { 
- 'adminer.dev':
+ 'adminer':
     ip           => '127.0.0.1',
     host_aliases => 'adminer.dev',
 }
-
+host { 
+ 'solrtest':
+    ip           => '127.0.0.1',
+    host_aliases => 'solrtest.dev',
+}
+host { 
+ 'solrtest2':
+    ip           => '127.0.0.1',
+    host_aliases => 'solrtest2.dev',
+}
 exec{
  'apache2_en_site_adminer':
     path    => '/usr/bin:/usr/sbin:/bin',
     command => 'a2ensite adminer',
     require => File['apache2.adminer-vhost']
 }
-
+exec{
+ 'apache2_en_site_solrtest':
+    path    => '/usr/bin:/usr/sbin:/bin',
+    command => 'a2ensite solrtest',
+    require => File['apache2.solrtest-vhost']
+}
+exec{
+ 'apache2_en_site_solrtest2':
+    path    => '/usr/bin:/usr/sbin:/bin',
+    command => 'a2ensite solrtest2',
+    require => File['apache2.solrtest2-vhost']
+}
 exec{
  'restart_apache2':
     path    => '/usr/bin:/usr/sbin:/bin',
@@ -175,3 +232,54 @@ exec{
     require => Exec['apache2_en_site_adminer']
 }
 
+
+exec{
+ 'download_solr':
+    path    => '/usr/bin:/usr/sbin:/bin',
+    cwd     => '/tmp',
+    creates  =>  "/tmp/solr-4.2.1.tgz",
+    command => 'wget http://apache.cu.be/lucene/solr/4.2.1/solr-4.2.1.tgz -O solr-4.2.1.tgz',
+    require => Exec['restart_apache2'],
+}
+exec { 'install_solr':
+  path    => '/usr/bin:/usr/sbin:/bin',
+  cwd => '/tmp',
+  command => 'tar xvzf solr-4.2.1.tgz  -C /home/vagrant/servers',
+  creates => '/home/vagrant/server/solr',
+  require => Exec['download_solr'],
+}
+
+#drupal things
+exec { 'drush-download-drupal':
+  path    => '/usr/bin:/usr/sbin:/bin',
+  cwd     => '/home/vagrant/code/web',
+  unless  => 'test -d /home/vagrant/code/web/solrtest',
+  command => 'git clone git://github.com/drupal/drupal.git solrtest && cd solrtest && git checkout -b drupal7 remotes/origin/7.x',
+  require => Exec['install_solr'],
+}
+#
+exec { 'drush-download-modules':
+  path    => '/usr/bin:/usr/sbin:/bin',
+  cwd     => '/home/vagrant/code/web/solrtest',
+  command => 'drush dl apachesolr devel features ctools strongarm node_export uuid drush_ctex_bonus --destination=/home/vagrant/code/web/solrtest/sites/all/modules',
+  require => Exec['drush-download-drupal'],
+}
+
+
+file { [ '/home/vagrant/code/web/solrtest/',
+         '/home/vagrant/code/web/solrtest/sites',
+         '/home/vagrant/code/web/solrtest/sites/all/',
+         '/home/vagrant/code/web/solrtest/sites/all/modules' ]:
+  ensure => directory,
+require => Exec['drush-download-modules'],
+}
+cron { 'drupal-cron-solrtest':
+  command => 'wget -O - -q -t 1 http://solrtest.dev/cron.php',
+  hour => inline_template('<%= name.hash % 24 %>'),
+  minute => "00",
+}
+exec { 'create-solrtest':
+  unless => '/usr/bin/mysql -uroot -proot drupal',
+  command => '/usr/bin/mysql -uroot -proot -e "create database drupal;"',
+  require => Exec['drush-download-modules'],
+}
